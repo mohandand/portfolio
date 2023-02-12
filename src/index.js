1a50b0e9d0740f5117ecc82b1f1d09789989c2c2
Mohan Dandigam <Mohan.Dandigam@vistex.com>
2023-02-11 22:17:21 -0600

    updated code

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 
-import 'https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css';
-
-import 'https://github.com/Remix-Design/RemixIcon/blob/master/fonts/remixicon.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'remixicon/fonts/remixicon.css';
 
 import App from './App';
 
